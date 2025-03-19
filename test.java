eb78a949ba4d04e8860c49b36de3d2469e06e93f
Pranaw-dev <juyal.pranaw@gmail.com>
2025-03-19 23:07:09 +0530

    new feature

--- test.java
+++ test.java
@@ -1,5 +1,12 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World");
+    }    
+    
+    public static void main1(String[] args) {
+        System.out.println("Hello World 1");
     }
+
+    public static void main2(String[] args) {
+        System.out.println("Hello World 2");}git classkkkkkkkkkkk
 }
