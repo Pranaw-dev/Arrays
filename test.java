64f15141343c505c7cd1e7559faa51e8d048c51f
Pranaw-dev <juyal.pranaw@gmail.com>
2025-03-20 12:19:54 +0530

    new class

--- test.java
+++ test.java
@@ -1,10 +1,16 @@
 public class test {
     public static void main(String[] args) {
-        int i = 1;
-        System.out.println(Integer.toString(i));
+        // int i = 1;
+        // System.out.println(Integer.toString(i));
 
         for (int j=0; j<10; j++) {
-            System.out.println(i);
+            System.out.println(j);
+        }
+
+            System.out.println("\n"+"==============================================");
+        int k = 0;
+        while (k++<10){
+            System.out.println("\n"+ k);
         }
     }    
 
