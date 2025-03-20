f0aa19fc0c22a51b5b47d38f7c69df8aa99c238f
Pranaw-dev <juyal.pranaw@gmail.com>
2025-03-20 19:46:03 +0530

    new feature

--- Hello.java
+++ Hello.java
@@ -1,13 +1,17 @@
+import java.util.*;
 public class Hello {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter the symbol: ");
-    String symbol = scanner.nextLine();
-    System.out.println("Enter the number of symbols: ");
-    int number = scanner.nextInt();
-    for (int i=0; i<number; i++)    {
-        for (int j=i; j<number; j++)    {
-            System.out.print(symbol);
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the symbol: ");
+        String symbol = scanner.nextLine();
+        // System.out.print("Enter the number of symbols: ");
+        int number = scanner.nextInt();
+        for (int i = 0; i < number; i++) {
+            for (int j = i; j < number; j++) {
+                System.out.print(symbol);
+            }
+            System.out.println();
         }
-        System.out.println();
     }
 }
